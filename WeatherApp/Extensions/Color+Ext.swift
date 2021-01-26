6b931dc40bab3a3698698a522888d86e54a7df6e
ruipasilva <ruipedro24@gmail.com>
2021-01-26 14:10:29 +0000

    Create Color+Ext.swift

---
+++ WeatherApp/Extensions/Color+Ext.swift
@@ -0,0 +1,18 @@
+//
+//  Color+Ext.swift
+//  WeatherApp
+//
+//  Created by Rui Silva on 25/01/2021.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    static let nightClearSkyTop = Color("nightClearSkyTop")
+    static let nightClearSkyBottom = Color("nightClearSkyBottom")
+    
+    static let dayClearSkyTop = Color("dayClearSkyTop")
+    static let dayClearSkyBottom = Color("dayClearSkyBottom")
+}
